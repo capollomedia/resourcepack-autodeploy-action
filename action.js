f153e645f4a724b186153746e2e80fc6cb81893e
LukynkaCZE <48604271+LukynkaCZE@users.noreply.github.com>
2022-08-25 09:01:40 +0200

    code formatting

--- action.js
+++ action.js
@@ -8,16 +8,16 @@ const axios = require('axios');
             var serverIP = core.getInput("serverIP");
             var serverKey = core.getInput("serverKey");
             var url = core.getInput("packURL");
-            core.info('Sending resourcepack to server with ip ' +serverIP +" and key " +serverKey +"...");
+            core.info('Sending resourcepack to server with ip ' + serverIP + " and key " + serverKey + "...");
             axios
-            .get("http://" +serverIP +'/updatepack/' +url, {
-                timeout: 1000
-            })
-            .then(res => {
-                core.info('ServerResourcpackDeployManager responded with' +res.status);
-                core.info(res);
-            })
-            .catch(error => core.setFailed(error.message));
+                .get("http://" + serverIP + '/updatepack/' + url, {
+                    timeout: 1000
+                })
+                .then(res => {
+                    core.info('ServerResourcpackDeployManager responded with' + res.status);
+                    core.info(res);
+                })
+                .catch(error => core.setFailed(error.message));
         } catch (error) {
             core.setFailed(error.message);
         }
