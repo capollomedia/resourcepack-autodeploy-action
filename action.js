5861dae0b7d8c78ac25f71f2134a02be62d148b1
LukynkaCZE <48604271+LukynkaCZE@users.noreply.github.com>
2022-08-25 08:44:56 +0200

    I..

--- action.js
+++ action.js
@@ -8,17 +8,16 @@ const axios = require('axios');
             var serverIP = core.getInput("serverIP");
             var serverKey = core.getInput("serverKey");
             var url = core.getInput("packURL");
-            
             core.info('Sending resourcepack to server with ip' +serverIP +" and key " +serverKey +"...");
             axios
             .get(serverIP +'/updatepack/' +url)
             .then(res => {
                 core.info('ServerResourcpackDeployManager responded with' +res.status);
                 core.info(res);
-            })           
-
-        } catch {
+            });
+        } catch (error) {
             core.setFailed(error.message);
         }
     }
-)
+
+)();
