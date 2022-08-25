81654098d9501cfcc5d88e917ab8fcba5d60284b
LukynkaCZE <48604271+LukynkaCZE@users.noreply.github.com>
2022-08-25 08:31:01 +0200

    Add arguments and rest of functionality

--- action.js
+++ action.js
@@ -1,12 +1,24 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const axios = require('axios');
 
 (
     async () => {
         try {
-            core.notice('Sending resourcepack to server..')
+            var serverIP = core.getInput("serverIP");
+            var serverKey = core.getInput("serverKey");
+            var url = core.getInput("packURL");
+            
+            core.notice('Sending resourcepack to server with ip' +serverIP +" and key " +serverKey +"...");
+            axios
+            .get(serverIP +'/updatepack/' +url)
+            .then(res => {
+                core.notice('ServerResourcpackDeployManager responded with' +res.status);
+                core.notice(res);
+            })           
+
         } catch {
-            core.setFailed(error.message)
+            core.setFailed(error.message);
         }
     }
 )
